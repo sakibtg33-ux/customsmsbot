afe14e1d4debb8a7b1ae69e365b1a170c9caf8a4
sakibtg33-ux <sakibtg33@gmail.com>
2026-06-28 23:13:36 +0600

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-# ============= সব তথ্য এখানে =============
-BOT_TOKEN = "8896805760:AAGt4CDbEdGP_Xedc9p_SpFu4d7rA3QOOSE"
-ADMIN_USER_ID = 1700797877   # ← আপনার আসল আইডি দিন (সংখ্যা)
-DEFAULT_API_KEY = "di80n58vVw6UDgQfH0bxtl3N3dR1i4yA6pfhPXEz"
-# =========================================
-
-API_URL = "https://api.sms.net.bd/sendsms"
