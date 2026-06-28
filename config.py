9ccd8d755224724ca740f6406cc588b7c948bc3d
sakibtg33-ux <sakibtg33@gmail.com>
2026-06-28 22:46:32 +0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# Telegram Bot Token (BotFather থেকে নিন)
+BOT_TOKEN = "8896805760:AAGt4CDbEdGP_Xedc9p_SpFu4d7rA3QOOSE"
+
+# আপনার টেলিগ্রাম ইউজার আইডি (অ্যাডমিন) – @userinfobot থেকে জানুন
+ADMIN_USER_ID = 1700797877  # সংখ্যা আকারে
+
+# sms.net.bd API endpoint
+API_URL = "https://api.sms.net.bd/sendsms"
